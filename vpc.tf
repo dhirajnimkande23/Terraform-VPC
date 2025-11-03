52ed2b110de17bfe73ce4f3e75be0a02d0cb3d78
Dhiraj Nimkande <dhirajnimkande23@gmail.com>
2025-11-03 21:04:11 +0530

    Create VPC with public and private subnets
    This configuration sets up a VPC with multiple public and private subnets, an internet gateway, and route tables for public access.

---
+++ vpc.tf
@@ -0,0 +1,101 @@
+resource "aws_vpc" "test-vpc" {
+    cidr_block = "10.0.0.0/16"
+    instance_tenancy = "default"
+    enable_dns_support = true
+    enable_dns_hostnames = true
+    tags = {
+        Name = "test-vpc"
+    }   
+}
+
+resource "aws_subnet" "test-pub-1" {
+  vpc_id = aws_vpc.test-vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = var.zone1 
+  tags = {
+    Name = "test-pub-1"
+  }
+}
+
+resource "aws_subnet" "test-pub-2" {
+  vpc_id = aws_vpc.test-vpc.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = var.ZONE2
+  tags = {
+    Name = "test-pub-2"
+  }     
+}
+
+resource "aws_subnet" "test-pub-3" {
+    vpc_id = aws_vpc.test-vpc.id
+    cidr_block = "10.0.3.0/24"
+    map_public_ip_on_launch = true
+    availability_zone = var.ZONE3
+    tags = {
+        Name = "test-pub-3"
+    }       
+  }
+
+  resource "aws_subnet" "test-priv-1" {
+    vpc_id = aws_vpc.test-vpc.id
+    cidr_block = "10.0.4.0/24"
+    map_public_ip_on_launch = true
+    availability_zone = var.ZONE1
+    tags = {
+        Name = "test-priv-1"
+    }
+  }
+resource "aws_subnet" "test-priv-2" {
+    vpc_id = aws_vpc.test-vpc.id
+    cidr_block = "10.0.5.0/24"
+    map_public_ip_on_launch = true
+    availability_zone = var.ZONE2
+    tags = {
+        Name = "test-priv-2"
+    }   
+}
+
+resource "aws_subnet" "test-priv-3" {
+    vpc_id = aws_vpc.test-vpc.id 
+    map_public_ip_on_launch = true
+    cidr_block = "10.0.6.0/24"
+    availability_zone = var.ZONE3
+    tags = {
+        Name = "test-priv-3"
+    }
+}
+
+resource "aws_internet_gateway" "test-igw" {
+    vpc_id = aws_vpc.test-vpc.id
+    tags = {
+        Name = "test-igw"
+    }
+}
+resource "aws_route_table" "test-public-rt" {
+    vpc_id = aws_vpc.test-vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.test-igw.id
+    }
+    tags = {
+        Name = "test-public-rt"
+    }
+}
+resource "aws_route_table_association" "test-pub-1-assoc" {
+    subnet_id = aws_subnet.test-pub-1.id
+    route_table_id = aws_route_table.test-public-rt.id
+
+}
+resource "aws_route_table-association" "test-pub-2-assoc" {
+    subnet_id = aws_subnet.test-pub-2.id
+    route_table_id = aws_route_table.test-public-rt.id
+
+}
+resource "aws_route_table_association" "test-pub-3-assoc" {
+    subnet_id = aws_subnet.test-pub-3.id
+    route_table_id = aws_route_table.test-public-rt.id
+
+}
+  
